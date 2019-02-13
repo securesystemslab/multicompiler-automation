a8a90fc2d00c093c3d39f752d6ad1f9e8ef861bc
Matt Dees <mdees@uci.edu>
2019-02-12 16:32:47 -0800

    Updated binutils configure script to support variables with spaces

--- configure_binutils.sh
+++ configure_binutils.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-PREFIX=$1
+PREFIX="$1"
 
 pushd binutils
-./configure --disable-werror --enable-plugins --enable-gold --prefix=$PREFIX --infodir="$PREFIX/share/info" 
+./configure --disable-werror --enable-plugins --enable-gold --prefix="$PREFIX" --infodir="$PREFIX/share/info" 
 touch .binutils_configured
 popd
 
