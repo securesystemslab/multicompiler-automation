1edc0a014255a8e2ba2decaf7640d0ca54bca17b
Matt Dees <mdees@uci.edu>
2019-06-24 12:44:05 -0700

    fetch_multicompiler.sh: fetch from 70 repos

--- fetch_multicompiler.sh
+++ fetch_multicompiler.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
 
-git clone -b cfar_38 git@github.com:/securesystemslab/multicompiler-priv.git llvm
+git clone -b cfar_70 git@github.com:/securesystemslab/multicompiler-priv.git llvm
 
-git clone -b cfar_38 git@github.com:/securesystemslab/multicompiler-clang-priv.git llvm/tools/clang
+git clone -b cfar_70 git@github.com:/securesystemslab/multicompiler-clang-priv.git llvm/tools/clang
 
-git clone -b cfar_38 git@github.com:/securesystemslab/multicompiler-compiler-rt-priv.git llvm/projects/compiler-rt
+git clone -b release_70 git@github.com:llvm-mirror/compiler-rt.git llvm/projects/compiler-rt
 
-git clone -b cfar_38 git@github.com:/securesystemslab/poolalloc-priv llvm/projects/poolalloc
+git clone -b cfar_70 git@github.com:/securesystemslab/poolalloc-priv llvm/projects/poolalloc
 
 git clone git@github.com:/rboggild/SVF llvm/projects/svf
 
