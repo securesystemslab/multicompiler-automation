de03cbdb7f6cfcf0ff8a8219dff986acc0a150ab
Matt Dees <mdees@uci.edu>
2019-06-28 21:07:40 +0000

    Currently multicompiler70 only supports debug build. Also changed compiler to GCC 7

--- setup_llvm.sh
+++ setup_llvm.sh
@@ -4,10 +4,10 @@ PREFIX="$1"
 
 mkdir -p llvm/build
 pushd llvm/build
-cmake .. -DCMAKE_C_COMPILER="/usr/bin/gcc-4.8"     \
-    -DCMAKE_CXX_COMPILER="/usr/bin/g++-4.8"   \
-    -DLLVM_TARGETS_TO_BUILD="X86"            \
-    -DCMAKE_INSTALL_PREFIX="$PREFIX"                \
+cmake .. -DCMAKE_C_COMPILER="/usr/bin/gcc-7"     \
+    -DCMAKE_CXX_COMPILER="/usr/bin/g++-7"   \
+    -DLLVM_TARGETS_TO_BUILD="X86"           	  \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX"              \
     -DCMAKE_BUILD_TYPE=Debug                    \
     -DLLVM_BINUTILS_INCDIR=../../binutils/include \
     -G Ninja ;                                    \
