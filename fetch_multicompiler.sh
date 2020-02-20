18cad35f61d9c175cd032408940750ac9fb31b80
Matt Dees <mdees@uci.edu>
2020-02-20 11:22:44 -0800

    Add --depth 1 to git pulls since the entire repo isn't needed

--- fetch_multicompiler.sh
+++ fetch_multicompiler.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 
-git clone -b code_layout_rando_70 git@github.com:/securesystemslab/multicompiler-priv.git llvm
+git clone -b code_layout_rando_70 git@github.com:/securesystemslab/multicompiler-priv.git --depth 1 llvm
 
-git clone -b code_layout_rando_70 git@github.com:/securesystemslab/multicompiler-clang-priv.git llvm/tools/clang
+git clone -b code_layout_rando_70 git@github.com:/securesystemslab/multicompiler-clang-priv.git --depth 1 llvm/tools/clang
 
-git clone -b code_layout_rando_70 git@github.com:securesystemslab/multicompiler-compiler-rt-priv.git
+git clone -b code_layout_rando_70 git@github.com:securesystemslab/multicompiler-compiler-rt-priv.git --depth 1 llvm/projects/compiler-rt 
