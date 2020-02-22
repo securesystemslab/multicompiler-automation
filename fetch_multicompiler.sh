1a3880b4b6986a869c4f263c9c88401c171bb464
Matt Dees <mdees@uci.edu>
2020-02-22 14:07:54 -0800

    Remove compiler-rt from projects

--- fetch_multicompiler.sh
+++ fetch_multicompiler.sh
@@ -5,6 +5,4 @@ git clone -b code_layout_rando_70 git@github.com:/securesystemslab/multicompiler
 
 git clone -b code_layout_rando_70 git@github.com:/securesystemslab/multicompiler-clang-priv.git --depth 1 llvm/tools/clang
 
-git clone -b code_layout_rando_70 git@github.com:securesystemslab/multicompiler-compiler-rt-priv.git --depth 1 llvm/projects/compiler-rt 
-
 git clone -b cfar_70 git@github.com:securesystemslab/poolalloc-priv.git --depth 1 llvm/projects/poolalloc
